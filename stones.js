b82c1592e15514f58904f1bd41cf2387f3bd6226
TarteraLover <ilovetartering@gmail.com>
2024-09-30 00:14:48 +0200

    Content+views

--- stones.js
+++ stones.js
@@ -1,5 +1,19 @@
 
 
 function UpdateViews(){
-    const view = document.getElementById();
+    const view = document.getElementById("views");
+    const startDate = new Date("2024-07-01T08:30:00");
+    const endDate = new Date();
+    const diffInMilliseconds = Math.abs(endDate - startDate);
+    const diffInMinutes = Math.floor(diffInMilliseconds / 60000);
+    view.innerHTML = (diffInMinutes *5)+"";
+    
 }
+
+function PageLoop(){
+    setInterval(() => {
+        UpdateViews();
+    }, 1000);
+}
+
+PageLoop();
